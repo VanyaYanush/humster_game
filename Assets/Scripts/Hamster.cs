0fb7103930a4b98e91a15aeb94f2ce66033bbf59
alexart2006 <alexart2006@yandex.ru>
2022-05-13 00:00:39 +0300

    Almost ready

--- Assets/Scripts/Hamster.cs
+++ Assets/Scripts/Hamster.cs
@@ -13,11 +13,13 @@ public class Hamster : MonoBehaviour
     void Update()
     {
         Transform camTransform = Camera.main.transform;
+
         Vector3 camPosition = new Vector3(camTransform.position.x - 1, transform.position.y, camTransform.position.z);
         Vector3 direction = (transform.position - camPosition).normalized;
         Vector3 forwardMovement = direction * Input.GetAxis("Vertical");
         Vector3 horizontalMovement = camTransform.right * Input.GetAxis("Horizontal");
         Vector3 movement = Vector3.ClampMagnitude(forwardMovement + horizontalMovement, 1);
+
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
     }
 }
